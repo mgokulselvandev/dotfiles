35b7b59f058814c019196ae57d5bdcf910425fb2
Gokul Selvan <mgokulselvan@gmail.com>
2025-11-25 11:36:44 +0000

    changes to script
    Fixed an issue where the changes made to the .rasi file from the script was dissappearing after some time, so made changes to the script to avoid this and make the changes to the file permanenet

--- .config/swww/choose_random_wallpaper.sh
+++ .config/swww/choose_random_wallpaper.sh
@@ -5,6 +5,9 @@ WALLPAPER_DIR="$HOME/Wallpapers"
 
 WALLPAPER=$(ls "$WALLPAPER_DIR" | shuf -n1)
 
-echo -e "*{ \n		wallpaperPath:url( \"$WALLPAPER_DIR/$WALLPAPER\" ,height);\n} " > ~/.config/rofi/wallpaperPath.rasi
+rm -f ~/.config/rofi/wallpaperPath.rasi
+touch ~/.config/rofi/wallpaperPath.rasi
+printf "*{ \n		wallpaperPath:url( \"$WALLPAPER_DIR/$WALLPAPER\" ,height);\n} " > ~/.config/rofi/wallpaperPath.rasi
+chmod 444 ~/.config/rofi/wallpaperPath.rasi
 
 [ -n "$WALLPAPER" ] && swww img -t random "$WALLPAPER_DIR/$WALLPAPER" 
--- .config/swww/choose_wallpaper.sh
+++ .config/swww/choose_wallpaper.sh
@@ -6,7 +6,10 @@ WALLPAPER_DIR="$HOME/Wallpapers"
 # Use rofi to select a wallpaper
 WALLPAPER=$(ls "$WALLPAPER_DIR" | rofi -dmenu -p "Select Wallpaper:")
 
-echo -e "*{ \n		wallpaperPath:url( \"$WALLPAPER_DIR/$WALLPAPER\" ,height);\n} " > ~/.config/rofi/wallpaperPath.rasi
+rm -f ~/.config/rofi/wallpaperPath.rasi
+touch ~/.config/rofi/wallpaperPath.rasi
+printf "*{ \n		wallpaperPath:url( \"$WALLPAPER_DIR/$WALLPAPER\" ,height);\n} " > ~/.config/rofi/wallpaperPath.rasi
+chmod 444 ~/.config/rofi/wallpaperPath.rasi
 
 # Set the wallpaper using swww
 [ -n "$WALLPAPER" ]&& swww img -t random "$WALLPAPER_DIR/$WALLPAPER" 
